b763a03ec00ebfdec0a130ec8d0adc69a7d91fbe
Thomas Sterren <thomas.sterren@gmail.com>
2021-04-22 16:39:57 +0200

    Updated logging behavior

--- src/cloudio/client/__init__.py
+++ src/cloudio/client/__init__.py
@@ -5,3 +5,8 @@
 import pkgutil
 
 __path__ = pkgutil.extend_path(__path__, __name__)
+
+import logging
+
+# Do not output logs if logging module is not configured
+logging.getLogger(__name__).addHandler(logging.NullHandler())
